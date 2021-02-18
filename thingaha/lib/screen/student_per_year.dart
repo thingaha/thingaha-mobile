a5f9ab8f6911cb271449dd77616b995b4e68494b
waithethtetoo <4itcourse@gmail.com>
2021-02-18 17:08:27 +0630

    Use reusable StudentUI

--- thingaha/lib/screen/student_per_year.dart
+++ thingaha/lib/screen/student_per_year.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:thingaha/helper/custom_appbar.dart';
-import 'package:thingaha/helper/custom_cardview.dart';
-import 'package:thingaha/helper/title_and_text_with_column.dart';
-import 'package:thingaha/model/student.dart';
-import 'package:thingaha/util/string_constants.dart';
-import 'package:thingaha/screen/student_info.dart';
+import 'package:thingaha/helper/student_ui.dart';
 
 class StudentPerYear extends StatefulWidget {
   final String year;
@@ -35,7 +31,7 @@ class _StudentPerYearState extends State<StudentPerYear> {
                   child: ListView.builder(
                     itemCount: students.length,
                     itemBuilder: (context, index) {
-                      return _buildStudentUI(context);
+                      return StudentUI();
                     },
                   ),
                 )
@@ -43,59 +39,5 @@ class _StudentPerYearState extends State<StudentPerYear> {
             ),
           ),
         ));
-  }
-
-  Widget _buildStudentUI(context) {
-    Student student = getStudent();
-
-    Widget photoWidget = Center(
-      child: Container(
-        width: 90.0,
-        height: 90.0,
-        child: Image.asset("images/ic_person.png"),
-        decoration: new BoxDecoration(
-          borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
-          border: new Border.all(
-            color: Colors.grey,
-            width: 2.0,
-          ),
-        ),
-      ),
-    );
-
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: CustomCardView(
-        onPress: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => StudentInfo()));
-        },
-        cardView: Container(
-          margin: EdgeInsets.all(10.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              photoWidget,
-              SizedBox(width: 25.0,),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    TitleAndTextWithColumn(title: txt_name, text: student.name),
-                    TitleAndTextWithColumn(title: txt_grade, text: student.grade),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Student getStudent() {
-    Student student = Student();
-    student.name = "Mg Zaw Zaw";
-    student.grade = "7";
-    return student;
   }
 }
